2a8f5fd83c6826e2d3b53253f4bf5a973dfe9642
vo_o <vo_o@etna-alternant.net>
2024-12-28 12:32:31 +0100

    [ADD] => setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/button";
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded-md shadow-md">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/button.tsx
@@ -0,0 +1,3 @@
+export const Button = () => {
+  return <div className="size-fit p-4 bg-red-400">HELLO WORLD</div>;
+};
